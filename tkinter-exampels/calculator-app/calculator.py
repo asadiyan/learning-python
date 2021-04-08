36071d00e58b1726d7dba389eb4eca27e7b40407
hamidreza <asadiyan.hamid@yahoo.com>
2021-04-08 20:55:41 +0430

    clear all function and delet function added

--- tkinter-exampels/calculator-app/calculator.py
+++ tkinter-exampels/calculator-app/calculator.py
@@ -14,6 +14,21 @@ def get_variables(num):
     i += 1
 
 
+def clear_all():
+    display.delete(0, "end")
+
+
+def undo():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0, new_string)
+    else:
+        clear_all()
+        display.insert(0, "ERROR")
+
+
 # adding the input feild
 display = tkinter.Entry(root)
 display.grid(row=1, columnspan=6, sticky="w"+"e")
@@ -32,7 +47,7 @@ tkinter.Button(root, text="8", command=lambda: get_variables(8)).grid(row=4, col
 tkinter.Button(root, text="9", command=lambda: get_variables(9)).grid(row=4, column=2)
 
 # adding other buttons to the Calculator
-tkinter.Button(root, text="AC").grid(row=5, column=0)
+tkinter.Button(root, text="AC", command=lambda: clear_all()).grid(row=5, column=0)
 tkinter.Button(root, text="0", command=lambda: get_variables(0)).grid(row=5, column=1)
 tkinter.Button(root, text="=").grid(row=5, column=2)
 
@@ -47,7 +62,7 @@ tkinter.Button(root, text="%").grid(row=3, column=4)
 tkinter.Button(root, text="(").grid(row=4, column=4)
 tkinter.Button(root, text="exp").grid(row=5, column=4)
 
-tkinter.Button(root, text="<-").grid(row=2, column=5)
+tkinter.Button(root, text="<-", command=lambda: undo()).grid(row=2, column=5)
 tkinter.Button(root, text="x!").grid(row=3, column=5)
 tkinter.Button(root, text=")").grid(row=4, column=5)
 tkinter.Button(root, text="^2").grid(row=5, column=5)
