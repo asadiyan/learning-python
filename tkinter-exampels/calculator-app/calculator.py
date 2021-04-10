03219d69a4a7a6499e84a3fe0becbb1ca16dde4a
hamidreza <asadiyan.hamid@yahoo.com>
2021-04-10 11:55:38 +0430

    the app is running

--- tkinter-exampels/calculator-app/calculator.py
+++ tkinter-exampels/calculator-app/calculator.py
@@ -14,6 +14,25 @@ def get_variables(num):
     i += 1
 
 
+def calculate():
+    entire_string = display.get()
+    try:
+        a = parser.expr(entire_string).compile()
+        result = eval(a)
+        clear_all()
+        display.insert(0, result)
+    except Exception:
+        clear_all()
+        display.insert(0, "ERROR")
+
+
+def get_operation(operator):
+    global i
+    length = len(operator)
+    display.insert(i, operator)
+    i += length
+
+
 def clear_all():
     display.delete(0, "end")
 
@@ -36,6 +55,7 @@ display.grid(row=1, columnspan=6, sticky="w"+"e")
 # adding buttons to the Calculator
 tkinter.Button(root, text="1", command=lambda: get_variables(1)).grid(row=2, column=0)
 tkinter.Button(root, text="2", command=lambda: get_variables(2)).grid(row=2, column=1)
+
 tkinter.Button(root, text="3", command=lambda: get_variables(3)).grid(row=2, column=2)
 
 tkinter.Button(root, text="4", command=lambda: get_variables(4)).grid(row=3, column=0)
@@ -49,23 +69,23 @@ tkinter.Button(root, text="9", command=lambda: get_variables(9)).grid(row=4, col
 # adding other buttons to the Calculator
 tkinter.Button(root, text="AC", command=lambda: clear_all()).grid(row=5, column=0)
 tkinter.Button(root, text="0", command=lambda: get_variables(0)).grid(row=5, column=1)
-tkinter.Button(root, text="=").grid(row=5, column=2)
+tkinter.Button(root, text="=", command=lambda: calculate()).grid(row=5, column=2)
 
-tkinter.Button(root, text="+").grid(row=2, column=3)
-tkinter.Button(root, text="-").grid(row=3, column=3)
-tkinter.Button(root, text="*").grid(row=4, column=3)
-tkinter.Button(root, text="/").grid(row=5, column=3)
+tkinter.Button(root, text="+", command=lambda: get_operation("+")).grid(row=2, column=3)
+tkinter.Button(root, text="-", command=lambda: get_operation("-")).grid(row=3, column=3)
+tkinter.Button(root, text="*", command=lambda: get_operation("*")).grid(row=4, column=3)
+tkinter.Button(root, text="/", command=lambda: get_operation("/")).grid(row=5, column=3)
 
 # adding new operation buttons to the Calculator
-tkinter.Button(root, text="pi").grid(row=2, column=4)
-tkinter.Button(root, text="%").grid(row=3, column=4)
-tkinter.Button(root, text="(").grid(row=4, column=4)
-tkinter.Button(root, text="exp").grid(row=5, column=4)
+tkinter.Button(root, text="pi", command=lambda: get_operation("*3.14")).grid(row=2, column=4)
+tkinter.Button(root, text="%", command=lambda: get_operation("%")).grid(row=3, column=4)
+tkinter.Button(root, text="(", command=lambda: get_operation("(")).grid(row=4, column=4)
+tkinter.Button(root, text="exp", command=lambda: get_operation("--")).grid(row=5, column=4)
 
 tkinter.Button(root, text="<-", command=lambda: undo()).grid(row=2, column=5)
 tkinter.Button(root, text="x!").grid(row=3, column=5)
-tkinter.Button(root, text=")").grid(row=4, column=5)
-tkinter.Button(root, text="^2").grid(row=5, column=5)
+tkinter.Button(root, text=")", command=lambda: get_operation(")")).grid(row=4, column=5)
+tkinter.Button(root, text="^2", command=lambda: get_operation("**")).grid(row=5, column=5)
 
 
 root.mainloop()
