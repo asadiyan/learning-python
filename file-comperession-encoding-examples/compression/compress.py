bb0a7db37c8cf0f7c7817fc3ab2f9e9b730a715c
hamidreza <asadiyan.hamid@yahoo.com>
2021-04-15 19:28:25 +0430

    the bug of compress.py solved

--- file-comperession-encoding-examples/compression/compress.py
+++ file-comperession-encoding-examples/compression/compress.py
@@ -9,16 +9,13 @@ data_bytes = bytes(data, "utf-8")
 
 # here we pas data_byte to compress function to compressing and save it in object or variable
 # the 9 up there is the level of compression which start`s from 0 to 9
-compress_data = zlib.compress(data_bytes, 9)
+compress_data = base64.b64encode(zlib.compress(data_bytes, 9))
 
-# now here we pass compressed data to b64encode function for encoding and save it in object or variable
-encoded_compressed_data = base64.b64encode(compress_data)
+# here we decode our compress_data because write mode just accept str file not encoded byte file 
+decoded_data = compress_data.decode("utf-8")
 
 # here we open a new file in write mode and save it in a object or variable
 compression_file = open("compressed.txt", "w")
-
-# here we decode our encoded file because write mode just accept str files or data
-decoded_data = encoded_compressed_data.decode()
 
 # now we write our decoded file into our compressed.txt file
 compression_file.write(decoded_data)
