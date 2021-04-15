d8351341d1275bb4e2f196024a27971ac176af36
hamidreza <asadiyan.hamid@yahoo.com>
2021-04-15 12:43:55 +0430

    ignoring some trash files

--- database-example/accept-data-from-user.py
+++ database-example/accept-data-from-user.py
@@ -13,10 +13,15 @@ def create():
 def insert_data():
     conn = psycopg2.connect(dbname="studentdb", user="postgres", password="example", host="localhost", port="5432")
     cur = conn.cursor()
-    cur.execute("""INSERT INTO student (NAME, AGE, ADDRESS) VALUES ('hamid', '25', 'tabriz');""")
+    name = input("enter your name:\n")
+    age = input("enter your age:\n")
+    address = input("enter your addres:\n")
+
+    query = """INSERT INTO student (NAME, AGE, ADDRESS) VALUES (%s, %s, %s);"""
+    cur.execute(query, (name, age, address))
     print("adding to table completed")
     conn.commit()
     conn.close()
 
-create()
+# create() 
 insert_data()
